a1a8beb01595f129b48564e9db115d2354914372
Mingyuyang-1 <Mingyu.Yang@amd.com>
2025-06-27 22:27:53 +0000

    [X-EcoMLA] Add support to FSDP

--- X-EcoMLA/trainer/kd_trainer.py
+++ X-EcoMLA/trainer/kd_trainer.py
@@ -8,10 +8,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 from accelerate.utils import is_deepspeed_available
+from transformers.models.llama.modeling_llama import LlamaDecoderLayer
 from transformers import AutoModelForCausalLM, PreTrainedModel, is_wandb_available
 
 from trl.models import PreTrainedModelWrapper
 from trl.trainer.sft_trainer import SFTTrainer
+
+from torch.distributed.fsdp.fully_sharded_data_parallel import FullyShardedDataParallel as FSDP
+from functools import partial
 
 from train_configs import SFTDistillConfig
 
@@ -42,11 +46,6 @@ class KDTrainer(SFTTrainer):
             )
         else:
             teacher_model_init_kwargs = args.teacher_model_init_kwargs
-            # teacher_model_init_kwargs["torch_dtype"] = (
-            #     teacher_model_init_kwargs["torch_dtype"]
-            #     if teacher_model_init_kwargs["torch_dtype"] in ["auto", None]
-            #     else getattr(torch, teacher_model_init_kwargs["torch_dtype"])
-            # )
 
         if isinstance(teacher_model, str):
             warnings.warn(
@@ -56,10 +55,8 @@ class KDTrainer(SFTTrainer):
             print("teacher_model_init_kwargs:", teacher_model_init_kwargs)
             teacher_model = AutoModelForCausalLM.from_pretrained(teacher_model, **teacher_model_init_kwargs)
 
-        if self.is_deepspeed_enabled:
-            self.teacher_model = self._prepare_deepspeed(teacher_model)
-        else:
-            self.teacher_model = self.accelerator.prepare_model(teacher_model, evaluation_mode=True)
+
+        self.teacher_model = self.prepare_fsdp(teacher_model, evaluation_mode=True)
 
         self.kl_weight = args.kl_weight
         self.ce_weight = args.ce_weight
@@ -102,44 +99,37 @@ class KDTrainer(SFTTrainer):
 
         # Return loss
         return (loss, outputs_student) if return_outputs else loss
+    
+    def prepare_fsdp(self, model, evaluation_mode=False):
+        # Adapted from accelerate: https://github.com/huggingface/accelerate/blob/739b135f8367becb67ffaada12fe76e3aa60fefd/src/accelerate/accelerator.py#L1421
+        # don't wrap it again
+        if not isinstance(model, FSDP):
+            # 
+            fsdp_plugin = deepcopy(self.accelerator.state.fsdp_plugin)
 
-    def _prepare_deepspeed(self, model: PreTrainedModelWrapper):
-        # Adapted from accelerate: https://github.com/huggingface/accelerate/blob/739b135f8367becb67ffaada12fe76e3aa60fefd/src/accelerate/accelerator.py#L1473
-        deepspeed_plugin = self.accelerator.state.deepspeed_plugin
-        config_kwargs = deepcopy(deepspeed_plugin.deepspeed_config)
+            fsdp_wrap_policy = partial(
+                fsdp_plugin.auto_wrap_policy, transformer_layer_cls=set(type(layer) for layer in model.model.layers)
+            )
+            # setattr(fsdp_plugin.mixed_precision_policy, '_module_classes_to_ignore', None)
+            kwargs = {
+                "sharding_strategy": fsdp_plugin.sharding_strategy or fsdp_plugin.reshard_after_forward,
+                "cpu_offload": fsdp_plugin.cpu_offload,
+                "auto_wrap_policy": fsdp_wrap_policy,
+                "mixed_precision": fsdp_plugin.mixed_precision_policy,
+                "sync_module_states": fsdp_plugin.sync_module_states,
+                "backward_prefetch": fsdp_plugin.backward_prefetch,
+                "forward_prefetch": fsdp_plugin.forward_prefetch,
+                "use_orig_params": fsdp_plugin.use_orig_params,
+                "param_init_fn": fsdp_plugin.param_init_fn,
+                "ignored_modules": fsdp_plugin.ignored_modules,
+                "limit_all_gathers": fsdp_plugin.limit_all_gathers,
+                "device_id": self.accelerator.device,
+            }
+            model = FSDP(model, **kwargs)
 
-        if model is not None:
-            if hasattr(model, "config"):
-                hidden_size = (
-                    max(model.config.hidden_sizes)
-                    if getattr(model.config, "hidden_sizes", None)
-                    else getattr(model.config, "hidden_size", None)
-                )
-                if hidden_size is not None and config_kwargs["zero_optimization"]["stage"] == 3:
-                    # Note that `stage3_prefetch_bucket_size` can produce DeepSpeed messages like: `Invalidate trace cache @ step 0: expected module 1, but got module 0`
-                    # This is expected and is not an error, see: https://github.com/microsoft/DeepSpeed/discussions/4081
-                    config_kwargs.update(
-                        {
-                            "zero_optimization.reduce_bucket_size": hidden_size * hidden_size,
-                            "zero_optimization.stage3_param_persistence_threshold": 10 * hidden_size,
-                            "zero_optimization.stage3_prefetch_bucket_size": 0.9 * hidden_size * hidden_size,
-                        }
-                    )
-
-        # If ZeRO-3 is used, we shard both the active and reference model.
-        # Otherwise, we assume the reference model fits in memory and is initialized on each device with ZeRO disabled (stage 0)
-        if config_kwargs["zero_optimization"]["stage"] != 3:
-            config_kwargs["zero_optimization"]["stage"] = 0
-        
-        # if you get an OOM error because of a super large teacher model, consider to try this
-        # config_kwargs['zero_optimization']['offload_param'] = {
-        #     'device': 'cpu',  # Offload parameters to CPU
-        #     'pin_memory': True,  # Enable pinned memory for faster CPU-GPU transfers
-        #     'nvme_path': None  # No NVMe offloading
-        # }
-        # config_kwargs["optimizer"] = {"type": None}
-        # print("config_kwargs:", config_kwargs)
-
-        model, *_ = deepspeed.initialize(model=model, config=config_kwargs)
-        model.eval()
+        if evaluation_mode:
+            model.eval()
+        else:
+            model.train()
         return model
+
